9f9f9c92d457c3de8b0d912654e10ba67c74b2a5
Mykhailo Orban <mikhail.orban@gmail.com>
2024-03-18 22:08:38 +0200

    new version test

--- src/main/java/org/example/DatabaseQueryService.java
+++ src/main/java/org/example/DatabaseQueryService.java
@@ -8,6 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class DatabaseQueryService {
@@ -27,6 +28,56 @@ public class DatabaseQueryService {
         }
         return result;
     }
+
+
+    public LongestProject findLongestProject() {
+        LongestProject result = null;
+        try (Connection connection = Database.getConnection();
+             Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(getQueryFromFile("sql/find_longest_projects.sql"));
+            if (resultSet.next()) {
+                String name = resultSet.getString("name");
+                int projectCount = resultSet.getInt("project_count");
+                result = new LongestProject(projectCount, name);
+            }
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    public List<MaxSalaryWorker> findMaxSalaryWorker() {
+        List<MaxSalaryWorker> result = new ArrayList<>();
+        try (Connection connection = Database.getConnection();
+             Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(getQueryFromFile("sql/find_max_salary_workers.sql"));
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                int workersCount = resultSet.getInt("project_count");
+                result.add(new MaxSalaryWorker(workersCount, name));
+            }
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public List<YoungestOldestWorker> YoungestOldestWorker() {
+        List<YoungestOldestWorker> result = new ArrayList<>();
+        try (Connection connection = Database.getConnection();
+             Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(getQueryFromFile("sql/find_youngest_eldest_workers.sql"));
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                Date Birthday = resultSet.getDate("BirthDay");
+                result.add(new YoungestOldestWorker(Birthday, name));
+            }
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
 
     private String getQueryFromFile(String filePath) throws IOException {
         StringBuilder query = new StringBuilder();
---
+++ src/main/java/org/example/MaxSalaryWorker.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class MaxSalaryWorker {
+    private  int salary;
+    private String name;
+
+    public MaxSalaryWorker (int salary,String name) {
+        this.salary = salary;
+        this.name = name;
+    }
+
+    @Override
+    public String toString () {
+        return "TestData{" +
+                "salary =" + salary +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
